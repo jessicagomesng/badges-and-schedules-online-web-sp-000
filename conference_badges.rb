3c0572f2f68a82eca609d5aa802671b0f464db79
Jessica Gomes-Ng <jessicagomesng@gmail.com>
2020-03-27 12:44:35 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,9 +10,7 @@ def batch_badge_creator(attendees)
 end
 
 def assign_rooms(attendees)
-  counter = 1
   attendees.each_with_index do |attendant, index|
     "Hello, #{attendant[index]}! You'll be assigned to room #{counter}"
-    counter += 1
   end
 end
