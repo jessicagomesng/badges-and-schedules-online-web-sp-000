7ed70292b1497503e955529602c1f27f8f863617
Jessica Gomes-Ng <jessicagomesng@gmail.com>
2020-03-27 12:50:28 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,10 +10,5 @@ def batch_badge_creator(attendees)
 end
 
 def assign_rooms(attendees)
-  counter = 1
-  while counter < 7
-    attendees.collect do |attendant|
-      "Hello, #{attendant}! You'll be assigned to room #{counter}!"
-    end
-  end 
+
 end
