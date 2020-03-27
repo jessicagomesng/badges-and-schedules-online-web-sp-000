a2bb487b27bfd470bc6ca4ac4d4150b8fb700f2c
Jessica Gomes-Ng <jessicagomesng@gmail.com>
2020-03-27 12:52:12 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,6 +11,6 @@ end
 
 def assign_rooms(attendees)
   attendees.each_with_index.map do |attendant, index|
-    "Hello, #{attendant}! You'll be assigned to #{index+1}!"
+    "Hello, #{attendant}! You'll be assigned to room #{index+1}!"
   end 
 end
